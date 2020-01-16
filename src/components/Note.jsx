5372bf6d364aed07a8fc972eb5cd7506bac24933
fguardiola <fernando.guardiola.888@gmail.com>
2020-01-16 10:38:54 +0100

    Keeper app part 2
    https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/17039230#content

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 import React from 'react';
-function Note(){
+function Note(props){
     return (
         <div className='note'>
-            <h1>This is the note title</h1>
+            <h1>{props.title}</h1>
             <p> 
-                This is the note content
+                {props.content}
             </p>
         </div>
     )
